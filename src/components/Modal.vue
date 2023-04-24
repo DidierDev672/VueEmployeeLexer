2d1c2c4f5a0d84b7ad75f06f4aece27d7dad7e4b
DidierDev672 <ariasdev672@gmail.com>
2023-04-23 20:22:27 -0500

    Modal or floating page for updating employee data

---
+++ src/components/Modal.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name: "view-modal"
+}
+</script>
+
+<template>
+  <div class="modal-container">
+    <div class="modal">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal-container{
+  position: fixed;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  place-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal {
+  background-color: white;
+  padding: 1rem;
+  max-width: 600px;
+  margin:auto;
+}
+
+button {
+  margin-top: 1rem;
+}
+</style>
