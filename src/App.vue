7bf351b6c45d47f2f8a9ae3975749431347c3610
DidierDev672 <ariasdev672@gmail.com>
2023-04-23 20:34:39 -0500

    Entry point of the web site and its component configuration

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterView } from "vue-router";
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+  <Header />
+<RouterView />
+</template>
+
+<style scoped>
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
